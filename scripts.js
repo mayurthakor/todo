6f7d751a434ce4887165b659d36436c91b7957af
yakibnordom <yakibnordom@protonmail.com>
2023-11-28 11:13:17 +0530

    updated code to support Local Storage

--- scripts.js
+++ scripts.js
@@ -7,17 +7,33 @@ const todoApp = {
     },
     methods: {
         addTodo() {
+            console.log("addTodo");
             if (!this.newTodo.text) {
                 this.todos.push({
                     text: this.newTodo,
                     done: false
                 })
+                localStorage.setItem('todos', JSON.stringify(this.todos));
                 this.newTodo = '';
             }
         },
-        removeTodo(index) {
-            this.todos.splice(index, 1)
+        clearAllToDos() {
+            console.log("clearAllToDos");
+            localStorage.clear();
+            this.todos = [];
+        },
+        storeToDos() {
+            localStorage.setItem('todos', JSON.stringify(this.todos));  
         }
+    },
+    removeTodo(index) {
+        this.todos.splice(index, 1)
+    },
+    created(){
+        this.todos = localStorage.getItem('todos') ? JSON.parse(localStorage.getItem('todos')) : [];
+    },
+    beforeUpdate(){
+        console.log('beforeUpdate');
     }
 }
 
